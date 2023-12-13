62e53f206304ac6165a52e9be364301fb7e14db3
“iron” <cbdi9812@gmail.com>
2023-12-13 11:24:52 +0800

    service example

--- app/src/main/java/com/example/hello_test/MainActivity.java
+++ app/src/main/java/com/example/hello_test/MainActivity.java
@@ -2,18 +2,23 @@ package com.example.hello_test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
 import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_content);
     }
 
 //    @Override
@@ -23,14 +28,49 @@ public class MainActivity extends AppCompatActivity {
 //    }true
 
     // Method to start the service
-    public void startService(View view) {
-        startService(new Intent(getBaseContext(), MyService.class));
-    }
+//    public void startService(View view) {
+//        startService(new Intent(getBaseContext(), MyService.class));
+//    }
 
     // Method to stop the service
-    public void stopService(View view) {
-        stopService(new Intent(getBaseContext(), MyService.class));
+//    public void stopService(View view) {
+//        stopService(new Intent(getBaseContext(), MyService.class));
+//    }
+
+    public void onClickAddName(View view) {
+        // Add a new student record
+        ContentValues values = new ContentValues();
+
+        values.put(StudentsProvider.NAME,
+                ((EditText)findViewById(R.id.editText2)).getText().toString());
+
+        values.put(StudentsProvider.GRADE,
+                ((EditText)findViewById(R.id.editText3)).getText().toString());
+
+        Uri uri = getContentResolver().insert(
+                StudentsProvider.CONTENT_URI, values);
+
+        Toast.makeText(getBaseContext(),
+                uri.toString(), Toast.LENGTH_LONG).show();
     }
 
+    public void onClickRetrieveStudents(View view) {
 
+        // Retrieve student records
+        String URL = "content://com.example.provider.College/students";
+
+        Uri students = Uri.parse(URL);
+        Cursor c = managedQuery(students, null, null, null, "name");
+
+        if (c.moveToFirst()) {
+            do{
+                Toast.makeText(this,
+                        c.getString(c.getColumnIndex(StudentsProvider._ID)) +
+                                ", " +  c.getString(c.getColumnIndex( StudentsProvider.NAME)) +
+                                ", " + c.getString(c.getColumnIndex( StudentsProvider.GRADE)),
+                        Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
+    }
 }
+
